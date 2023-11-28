b3f26be0666b26f441340c5fcafd98e5339a5b6c
tetraphoz <tetraphosphorus@gmail.com>
2023-11-28 13:14:53 -0600

    Cambiar color del header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,13 +14,13 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-black">
+    <body class="font-sans antialiased bg-black flex justify center items-center h-screen">
+        <div class="min-h-screen">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
             @if (isset($header))
-                <header class="bg-white shadow">
+                <header class="bg-black shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
