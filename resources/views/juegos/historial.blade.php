166e9c1c740b7433bf09a349b8141946f4e7c4ce
tetraphoz <tetraphosphorus@gmail.com>
2023-11-29 13:16:41 -0600

    Mostrar historial

--- resources/views/juegos/historial.blade.php
+++ resources/views/juegos/historial.blade.php
@@ -2,7 +2,7 @@
 <table class="border border-gray-300">
     <thead>
         <tr>
-            <th class="py-2 px-4 border-b">Fecha</th>
+            <th cklass="py-2 px-4 border-b">Fecha</th>
             <th class="py-2 px-4 border-b">Jugador 1</th>
             <th class="py-2 px-4 border-b">Jugador 2</th>
             <th class="py-2 px-4 border-b">Resultado Jugador 1</th>
@@ -10,13 +10,13 @@
         </tr>
     </thead>
     <tbody>
-        @foreach($historial ?? [] as $juego)
+        @foreach($historial as $partida)
             <tr class="hover:bg-gray-100">
-                <td class="py-2 px-4 border-b">{{ $juego->created_at }}</td>
-                <td class="py-2 px-4 border-b">{{ $juego->jugador1->name }}</td>
-                <td class="py-2 px-4 border-b">{{ $juego->jugador2->name }}</td>
-                <td class="py-2 px-4 border-b">{{ $juego->resultado_jugador1 }}</td>
-                <td class="py-2 px-4 border-b">{{ $juego->resultado_jugador2 }}</td>
+                <td class="py-2 px-4 border-b">{{ $partida->created_at }}</td>
+                <td class="py-2 px-4 border-b">{{ $partida->jugador1->name }}</td>
+                <td class="py-2 px-4 border-b">{{ $partida->jugador2->name }}</td>
+                <td class="py-2 px-4 border-b">{{ $partida->resultado_jugador1 }}</td>
+                <td class="py-2 px-4 border-b">{{ $partida->resultado_jugador2 }}</td>
             </tr>
         @endforeach
     </tbody>
